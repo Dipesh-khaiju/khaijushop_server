2d915bbe3cb08697fafe347405acbe272ec31906
Dipesh Khaiju <khaijudipesh@gmail.com>
2024-03-25 09:56:54 +0545

    atob

--- middleware/handleEsewaSuccess.js
+++ middleware/handleEsewaSuccess.js
@@ -3,10 +3,11 @@ import createSignature from "../utility/Hashing.js";
 const handleEsewaSuccess = (req, res, next) => {
   try {
     const { data } = req.query;
-    const decodedData = JSON.parse(
-      Buffer.from(data, "base64").toString("utf-8")
-    );
+  
+    const decodedData = atob(data) // atob sunction decodes base 64  encoded string into javascript
+    
     console.log(decodedData);
+
     if (decodedData?.status !== "COMPLETE") {
       return res.json({ message: "Payment Failed" });
     }
@@ -15,14 +16,17 @@ const handleEsewaSuccess = (req, res, next) => {
       .map((field) => `${field}=${decodedData[field] || ""}`)
       .join(",");
     console.log(message);
+
     const signature = createSignature(message);
-    if (signature !== decodedData?.signature) {
+    if (signature !== decodedData?.signature) {  //checks if the generated signature matches the signature value in the decodedData
       res.json({ message: "Payment Failed" });
     }
     req.transaction_uuid = decodedData?.transaction_uuid;
     req.transaction_code = decodedData?.transaction_code;
     next();
-  } catch (error) {
+  }
+  
+  catch (error) {
     console.log(error);
     return res.json({ message: "Error Occured" });
   }
